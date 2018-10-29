af096843b67797bec5481e825d89a54ddb6b24e3
Bharat Arya <arya.bharat@rocketmail.com>
2018-10-29 15:56:49 +0530

    Update create.c

--- create.c
+++ create.c
@@ -5,6 +5,9 @@
 #include<stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <unistd.h>      // Header file for Commit command
+#include <limits.h>     // Header File for commit command "PATH_MAX"
+#include <dirent.h>
 
 // Create a new file if already exist give error message.
 
@@ -22,6 +25,46 @@ void create_new_file(char* name[])
       }
 }
 
+void commit()
+{
+  /*
+  Code to print current directory
+  */
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+    printf("Current working dir: %s\n", cwd);
+
+    /****************  PRINT ALL THE FILES IN DIRECTORY  *****************/
+
+    struct dirent *de;  // Pointer for directory entry
+
+    // opendir() returns a pointer of DIR type.
+    DIR *dr = opendir(cwd);
+
+    if (dr == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not open current directory" );
+        return;
+    }
+
+    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
+    // for readdir()
+      printf("ALL the files in the repo are:");
+    while ((de = readdir(dr)) != NULL)
+            printf("%s\n", de->d_name);
+
+    closedir(dr);
+  /*  if (getcwd(cwd, sizeof(cwd)) != NULL)
+      {
+          printf("Current working dir: %s\n", cwd);
+      }
+     else
+       {
+          perror("getcwd() error");
+          return 1;
+       }*/
+}
+
 int main(int argc,char* argv[])
 {
     int counter;
@@ -30,19 +73,31 @@ int main(int argc,char* argv[])
    // argc is 3 for new directory..
     if(argc==3)
     {
-      if(!strcmp(argv[1], ".init"))
-      {
-        create_new_file(argv);
-      }
-      else
-      {
-        printf("Error %s\n",argv[1]);
-      }
+        if(!strcmp(argv[1], ".init"))
+        {
+          create_new_file(argv);
+        }
+
+        else
+        {
+          printf("Error %s\n",argv[1]);
+        }
     }
-    else
+    else if(argc == 2)
      {
-         printf("Error %s\n",argv[1]);
+       if(!strcmp(argv[1], "commit"))
+         {
+             commit();
+         }
+         else
+         {
+           printf("Error %s\n",argv[1]);
+         }
      }
+     else
+      {
+          printf("Error %s\n",argv[1]);
+      }
 
    /*
    code to make a new directory in the name direcorty.
