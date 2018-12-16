0da26efca070cae9187b6f76060345bd138196c8
Bharat Arya <arya.bharat@rocketmail.com>
2018-12-17 01:54:40 +0530

    Add files via upload
    last moment changes

--- Difference.h
+++ Difference.h
@@ -1,58 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
 #define MAX 200
 
-int maximum(int x, int y); 
+int maximum(int x, int y);
 
 void lcs(char *X, char *Y, int ctr) {
 
-	int m = strlen(X); 
-	int n = strlen(Y); 
+	int m = strlen(X);
+	int n = strlen(Y);
 	int L[m+1][n+1];
-	
-	//Creating the memorization table
-	for (int i=0; i<=m; i++) { 
-		for (int j=0; j<=n; j++) { 
-			if (i == 0 || j == 0) 
-				L[i][j] = 0; 
-			else if (X[i-1] == Y[j-1]) 
-				L[i][j] = L[i-1][j-1] + 1; 
-			else L[i][j] = maximum(L[i-1][j], L[i][j-1]); 
-		} 
+
+	for (int i=0; i<=m; i++) {
+		for (int j=0; j<=n; j++) {
+			if (i == 0 || j == 0)
+				L[i][j] = 0;
+			else if (X[i-1] == Y[j-1])
+				L[i][j] = L[i-1][j-1] + 1;
+			else L[i][j] = maximum(L[i-1][j], L[i][j-1]);
+		}
 	}
 
-	int ix = L[m][n]; 
-  
-	char lcs[ix+1]; 
+	int ix = L[m][n];
+	char lcs[ix+1];
 	char addition[m];
 	char deletion[n];
 	lcs[ix] = '\0';
 
-	//Initializing variable sized character array named addition 
+	//Initializing variable sized character array named addition
 	for (int i = 0; i < m; ++i) {
 		addition[i] = '0';
 	}
-	
+
 	//Initializing variable sized character array named deletion
 	for (int i = 0; i < n; ++i) {
 		deletion[i] = '0';
 	}
-  
-	int i = m, j = n; 
-	while (i > 0 && j > 0) { 
+
+	int i = m, j = n;
+	while (i > 0 && j > 0) {
   		if (X[i-1] == Y[j-1]) {
-			lcs[ix-1] = X[i-1];  //Including in lcs
-			i--; 
-			j--; 
-			ix--; 
-		} 
-		else if (L[i-1][j] > L[i][j-1]) {
-			addition[i-1] = X[i-1]; // Including in addition
+			lcs[ix-1] = X[i-1];
 			i--;
+			j--;
+			ix--;
+			//printf("%c", lcs[ix-1]);
+		}
+		else if (L[i-1][j] > L[i][j-1]) {
+			//printf("At line %d :", ctr);
+			//printf("\033[1;32m%c\033[0m", X[i-1]);
+			addition[i-1] = X[i-1];
+			//lcs[ix-1] = '';  // Including the given character in addition
+			i--;
+			//printf("\n");
 		}
 		else {
-			deletion[j-1] = Y[j-1];  // Including in deletion
+			//printf("At line %d :", ctr);
+			//printf("\033[1;31m%c\033[0m", Y[j-1]);
+			deletion[j-1] = Y[j-1];
+			//lcs[ix-1] = ' ';  // Including the given character in deletion
 			j--;
+			// printf("\n");
 		}
 	}
+	//printf("At line %d :", ctr);
 
 	int q = 0;
 	int r = 0;
@@ -65,43 +77,55 @@ void lcs(char *X, char *Y, int ctr) {
 	 			//printf("%s", lcs);
 	 			r++;
 	 		}
-	 		printf("\033[1;32m%c\033[0m",addition[i]); //Printing addition
+	 		printf("\033[1;32m%c\033[0m",addition[i]);
 	 	}
 	}
 
-	if(strlen(lcs) == 0){
-		printf("At line %d :", ctr);
-		printf("\033[1;32m%s\033[0m", X);
-	}
+	//printf("%s\n", lcs);
 
 	int p = 0;
 	for (int i = 0; i < n; ++i) {
+		//printf("\nAt line %d :", ctr);
 		if (deletion[i] != '0') {
 			if (p == 0) {
 			printf("\nAt line %d :", ctr);
 			p++;
 		}
-			printf("\033[1;31m%c\033[0m",deletion[i]); //Printing deletion
+			printf("\033[1;31m%c\033[0m",deletion[i]);
 		}
 	}
 
+	// if(strlen(lcs) == 0){
+	// 	printf("At line %d :", ctr);
+	// 	printf("\033[1;31m%s\033[0m", Y);
+	// }
+
+	if(strlen(lcs) == 0){
+		printf("At line %d :", ctr);
+		printf("\033[1;32m%s\033[0m", Y);
+	}
+
+	if(strlen(lcs) == 0){
+		printf("At line %d :", ctr);
+		printf("\033[1;31m%s\033[0m", X);
+	}
+
    //Printing the lcs
+
 	if (q == 0) {
 		printf("\nAt line %d :", ctr);
 		printf("%s\n", lcs);
 		q++;
 	}
+
 }
 
-
-//Returns the maximum of 2 integers
 int maximum(int x, int y) {
 	return (x > y) ? x : y;
-} 
-  
-//Open and read the given files
-void openfiles(char *file1, char *file2) { 
-  
+}
+
+void openfiles(char *file1, char *file2) {
+
   FILE *ptr1, *ptr2;
 
 	ptr1 = fopen(file1, "r");
@@ -115,7 +139,7 @@ void openfiles(char *file1, char *file2) {
 	if (ptr1 == NULL) {
 		printf("New file was created. \n");
 		exit(0);
-	} 
+	}
 
 	fseek(ptr1, 0, SEEK_END);
 	int len1 = ftell(ptr1);
@@ -126,7 +150,7 @@ void openfiles(char *file1, char *file2) {
 	if (ptr2 == NULL) {
 		printf("File was deleted. \n");
 		exit(0);
-	} 
+	}
 
 	fseek(ptr2, 0, SEEK_END);
 	int len2 = ftell(ptr2);
@@ -141,13 +165,13 @@ void openfiles(char *file1, char *file2) {
 	int count1 = 1, count2 = 1;
 
 	for (c = getc(ptr1); c != EOF; c = getc(ptr1))  {
-        if (c == '\n') 
-            count1 = count1 + 1; 
+        if (c == '\n')
+            count1 = count1 + 1;
 	}
 
     for (c = getc(ptr2); c != EOF; c = getc(ptr2)) {
-        if (c == '\n') 
-            count2 = count2 + 1; 
+        if (c == '\n')
+            count2 = count2 + 1;
     }
 
     rewind(ptr1);
@@ -158,53 +182,57 @@ void openfiles(char *file1, char *file2) {
 	while ((fgets (str1, MAX, ptr1)!= NULL) && (fgets (str2, MAX, ptr2)!=NULL)) {
 		lcs(str1, str2, ctr);
 		ctr++;
-	
+		//printf("\n");
 	}
 
 	rewind(ptr1);
     rewind(ptr2);
-    
+
     int cmt = 0;
     int cnt = 0;
 
     int c_ounter;
 
-	if (count1 > count2) {
-	
-		char line[500]; /*Suitable maximum line size */
+	if (count1 >= count2) {
+		// printf("%d\n", count1);
+		// printf("%d\n", count2);
+		char line[500]; /* or other suitable maximum line size */
         while (fgets(line, sizeof line, ptr1) != NULL) /* read a line */{
-            if (cmt >= count2)
-            {   
+            if (cmt >= count2-1)
+            {
             	cmt++;
             	printf("\nAt line %d:", cmt);
-                
+                //use line or in a function return it
+                //            //in case of a return first close the file with "fclose(file);"
             printf("\033[1;32m%s\033[0m",line);
-            }   
+            }
             else
-            {   
+            {
                 cmt++;
-            }   
-        } 
+            }
+        }
 	} else if (count2 > count1) {
-		
-		char line[500]; /*suitable maximum line size */
+		// printf("%d\n", count1);
+		// printf("%d\n", count2);
+		char line[500]; /* or other suitable maximum line size */
         while (fgets(line, sizeof line, ptr2) != NULL) /* read a line */{
-            if (cmt >= count1)
-            {   
+            if (cmt >= count1-1)
+            {
             	cmt++;
             	printf("\nAt line %d:", cmt);
+                //use line or in a function return it
+                //            //in case of a return first close the file with "fclose(file);"
             printf("\033[1;31m%s\033[0m",line);
-            }   
+            }
             else
-            {   
+            {
                 cmt++;
-            }   
-          }
+            }
+        }
 	}
 	else printf("\n");
 	printf("\n");
-	
 	fclose(ptr1);
 	fclose(ptr2);
-   
-} 
+
+}
